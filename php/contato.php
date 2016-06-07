1c4839004124f20ce9b2ec10911239221a8ca806
Beeuz Produções Visuais <BeeuzOne@Beeuzs-Mac-mini.local>
2016-06-07 11:08:48 -0300

    quase la

---
+++ php/contato.php
@@ -0,0 +1,62 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <title>Enviar E-mail com PHP</title>
+</head>
+<body>
+    
+      <form action="enviar.php" name="form_contato" method="post" id="contact-form" >
+
+      <div class="container-fluid" style="color: #fff; background: #60286d">
+
+                        <div class="container" style="margin:0; padding: 0">
+                        
+                        <h3>Contato para duvidas</h3><br>
+                            <div class="row" style="">
+                   
+                        <article class="form-detached">
+                                <div class="alert alert-success hidden" id="contact-alert-success">
+                                    <strong>Success!</strong> Thank you for your message. Reply will be in a while!
+                                </div>
+                                <div class="alert alert-danger hidden" id="contact-alert-error">
+                                    <strong>Error!</strong> Something went wrong sending your message.
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="sr-only" for="name"></label>
+                                    <input type="text" class="form-control" value="" placeholder="Nome" data-msg-required="Please enter your name." name="nome" id="nome">
+                                </div>
+
+                                 <div class="form-group">
+                                    <label class="sr-only" for="cpfcnpj"></label>
+                                    <input type="text" class="form-control" value="" placeholder="CPF ou CNPJ" data-msg-required="Please enter your name." name="cpfcnpj" id="cpfcnpj">
+                                </div>
+                                    
+                                <div class="form-group">
+                                    <label class="sr-only" for="cidade"></label>
+                                    <input type="text" value="" placeholder="Cidade" data-msg-required="Please enter your email address." data-msg-email="Please enter a valid email address." class="form-control" name="cidade" id="cidade">
+                                </div>
+
+                                 <div class="form-group">
+                                    <label class="sr-only" for="telefone"></label>
+                                    <input type="tel" value="" placeholder="Telefone ou Celular" data-msg-required="Please enter your email address." data-msg-email="Please enter a valid email address." class="form-control" name="telefone" id="telefone">
+                                </div>
+
+                                 <div class="form-group">
+                                    <label class="sr-only" for="email"></label>
+                                    <input type="mail" class="form-control" value="" placeholder="E-mail" data-msg-required="Please enter your email." name="email" id="email" style="width:">
+                                </div>
+
+                                <div class="form-group">
+                                  <label class="sr-only" for="name"></label>
+                                  <textarea class="form-control" rows="3" placeholder="Mensagem" name="mensagem" id="mensagem" style="border-radius: 5px"></textarea>
+                                </div>
+                                <input type="submit" class="campo_submit" style="background:#128D88; color: #fff;border:none; ;border-radius: 5px; font-size: 28px; padding-top:10px; margin:0" value="Enviar" />
+                  </form> 
+
+                </div>
+              </div>
+            </div>
+       
+</body>
+</html>
---
+++ php/enviar2.php
@@ -0,0 +1,76 @@
+﻿<?php
+//Variaveis
+
+$nome 		= $_POST['nome'];
+$cpfcnpj 	= $_POST['cpfcnpj'];
+$cidade		= $_POST['cidade'];
+$telefone 	= $_POST['telefone'];
+$email 		= $_POST['email'];
+$mensagem 	= $_POST['mensagem'];
+
+$data_envio = date('d/m/Y');
+$hora_envio = date('H:i:s');
+$emaildestino = 'gustavohquinalha@gmail.com';
+
+// -------------
+
+// Compo E-mail
+
+	
+	$arquivo = "
+	<html>
+        <table width='100%' border='0' cellpadding='1' cellspacing='1'>
+            <tr>
+              <td>
+			    <tr>
+                 <td width='500'>Nome: <b>$nome</b></td>
+                </tr>
+                <tr>
+                  <td width='320'>CPF ou CNPJ: <b>$cpfcnpj</b></td>
+	            </tr>
+	            <tr>
+                  <td width='320'>Cidade: <b>$cidade</b></td>
+	            </tr>
+                <tr>
+                  <td width='320'>E-mail: <b>$email</b></td>
+	            </tr>
+				<tr>
+                  <td width='320'>Telefone: <b>$telefone</b></td>
+                </tr>
+				<tr>
+                  <td width='320'>Mensagem: $mensagem</td>
+                </tr>
+            </td>
+          </tr>  
+          <tr>
+            <td>Este e-mail foi enviado em <b>$data_envio</b> &agrave;s <b>$hora_envio</b></td>
+          </tr>
+        </table>
+    </html>
+	";
+
+// -------------------------
+
+//enviar
+	
+	// emails para quem será enviado o formulário
+	$destino = 'gustavohquinalha@gmail.com';
+	$assunto = "Contato OnPago 2";
+
+	// É necessário indicar que o formato do e-mail é html
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'From:' . $nome;
+	//$headers .= "Bcc: $EmailPadrao\r\n";
+	
+	$enviaremail = mail($destino, $assunto, $arquivo, $headers);
+	if($enviaremail){
+	$mgm = "E-MAIL ENVIADO COM SUCESSO! <br>";
+	echo " <meta http-equiv='refresh' content='3;URL=../contato1.html'>";
+	echo $mgm;
+	} else {
+	$mgm = "ERRO AO ENVIAR E-MAIL!";
+	echo "";
+	echo $mgm;
+	}
+?>
